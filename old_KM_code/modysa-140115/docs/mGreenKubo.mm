2fa791b84c077c9706bcdcee94fcc661dec2cfb1
lessh2506 <shvetslesia2506@gmail.com>
2024-08-07 21:40:55 +0200

    del

--- old_KM_code/modysa-140115/docs/mGreenKubo.mm
+++
@@ -1,4 +0,0 @@
-
-        # in tip3p sampled every 1fs, settings were as follows:
-        # shift = 10, span = 3000, step = 0.001
-        # skip can be 10 (changes in eta as compared to skip=1 are minor)
--- old_KM_code/modysa-140115/modysa/io/gromacs/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ["gmxiobase", "xtcio", "trnio", "gmxutl"]
